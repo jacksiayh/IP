b60417cbaea0717f7f5525193bc074b463457c82
jacksiayh <s10257192@connect.np.edu.sg>
2024-02-08 12:00:52 +0800

    commiting to see if this fixes an error

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,19 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the form element
     var form = document.querySelector('form');
 
-    // Add a submit event listener to the form
     form.addEventListener('submit', function (event) {
-        // Prevent the default form submission
         event.preventDefault();
 
-        // Clear the input fields
         clearForm();
 
         window.location.href = 'redirect.html';
     });
 
-    // Function to clear the form fields, excluding the submit button
     function clearForm() {
         var formInputs = form.querySelectorAll('input:not([type="submit"])');
         formInputs.forEach(function (input) {
@@ -22,10 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if (localStorage.getItem('redirected') === 'true') {
-        // Display an alert indicating successful transaction
         alert('Successful transaction!');
 
-        // Reset the 'redirected' variable to false
+        localStorage.removeItem('shoppingCart');
+
         localStorage.setItem('redirected', 'false');
+
+        window.location.href = 'cart.html';
     }
 });
